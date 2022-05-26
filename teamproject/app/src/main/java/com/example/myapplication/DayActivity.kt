fa5f36e8700e60189ddbab6aca715399229a3db9
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-05-27 08:14:33 +0900

    calendar and todolist

--- teamproject/app/src/main/java/com/example/myapplication/DayActivity.kt
+++ teamproject/app/src/main/java/com/example/myapplication/DayActivity.kt
@@ -2,13 +2,30 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.ActivityDayBinding
 
 class DayActivity : AppCompatActivity() {
+    var data:ArrayList<Tododata> = ArrayList()
     lateinit var binding: ActivityDayBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDayBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initData()
+        initRecyclerView()
+    }
+
+    private fun initData() {
+        data.add(Tododata("씻고 준비하기"))
+        data.add(Tododata("다시 잠들기"))
+        data.add(Tododata("코딩하기"))
+        data.add(Tododata("데이트하기"))
+    }
+
+    private fun initRecyclerView() {
+        binding.todoRecyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        binding.todoRecyclerView.adapter = TodoAdapter(data)
     }
 }
--- teamproject/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ teamproject/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,12 +1,17 @@
 //메인 화면
 package com.example.myapplication
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.prolificinteractive.materialcalendarview.format.ArrayWeekDayFormatter
+import com.prolificinteractive.materialcalendarview.format.MonthArrayTitleFormatter
+import java.time.DayOfWeek
 
 
 class MainActivity : AppCompatActivity() {
@@ -32,6 +37,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initLayout() {
+        binding.calendar.apply {
+            //처음 날짜를 월요일로 바꾸어주기
+//            state()
+//                .edit()
+//                .setFirstDayOfWeek(DayOfWeek.of(Calendar.MONDAY))
+//                .commit();
+            // 월,일 style 수정
+            setTitleFormatter(MonthArrayTitleFormatter(getResources().getTextArray(R.array.custom_months)));
+            setWeekDayFormatter(ArrayWeekDayFormatter(getResources().getTextArray(R.array.custom_weekdays)));
+            setHeaderTextAppearance(R.style.CalendarWidgetHeader);
+            setOnDateLongClickListener { widget, date ->
+                val intent = Intent(this@MainActivity,DayActivity::class.java)
+                startActivity(intent)
+            }
+        }
+
         //깰시간 정해주는 버튼
         binding.button1.setOnClickListener {
             val intent = Intent(this, RecommendupActivity::class.java)
