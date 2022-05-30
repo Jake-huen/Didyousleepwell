62ffc85d2038b2e4fc481a5ac1c96ae29e58b703
Nbowow <nbo5548@naver.com>
2022-05-30 22:32:28 +0900

    Recommenddown, up 시간 계산 및 추천 완료

--- teamproject/app/src/main/java/com/example/myapplication/RecommenddownActivity2.kt
+++ teamproject/app/src/main/java/com/example/myapplication/RecommenddownActivity2.kt
@@ -7,6 +7,7 @@ import com.example.myapplication.databinding.ActivityRecommenddown2Binding
 
 class RecommenddownActivity2 : AppCompatActivity() {
     lateinit var binding : ActivityRecommenddown2Binding
+    private var dbHelper = DBHelper(this, "dysw.db", null, 1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecommenddown2Binding.inflate(layoutInflater)
@@ -15,17 +16,24 @@ class RecommenddownActivity2 : AppCompatActivity() {
     }
 
     private fun initLayout() {
-        //나이에 따라서 깰시간 추천해주면 된다.
-        var hour = intent.getIntExtra("hour", 0)
-        var minute = intent.getIntExtra("minute", 0) + 17//평균 잠드는데 까지 걸리는 시간 더해주면 됨.
 
-        if(minute>59) {
-            hour += 1
-            minute -= 60
+        val age = dbHelper.getAge()
+        //어짜피 한시간 반 고정이라 나이값은 딱히 필요없을듯..
+        var sleepTime = dbHelper.getTimeFallSleep()
+
+        var hour = intent.getIntExtra("hour", 0)
+        var minute = intent.getIntExtra("minute", 0) + sleepTime//평균 잠드는데 까지 걸리는 시간 더해주면 됨.
+
+        while(minute>59){
+            if(minute>59) {
+                hour += 1
+                minute -= 60
+            }
+            if(hour>23) {
+                hour -= 24
+            }
         }
-        if(hour>23) {
-            hour -= 24
-        }
+
 
         val timeString = hour.toString() + "시 " + minute.toString() + "분"
 
@@ -33,48 +41,61 @@ class RecommenddownActivity2 : AppCompatActivity() {
 
         var hour2 = hour+1
         var minute2 = minute+30
-        if(minute2>59) {
-            hour2 += 1
-            minute2 -= 60
+
+        while(minute2>59){
+            if(minute2>59) {
+                hour2 += 1
+                minute2 -= 60
+            }
+            if(hour2>23) {
+                hour2 -= 24
+            }
         }
-        if(hour2>23) {
-            hour2 -= 24
-        }
+
         val timeString2 = hour2.toString() + "시" + minute2.toString() + "분"
         binding.timeTextView2.text = timeString2
 
         var hour3 = hour2+1
         var minute3 = minute2+30
-        if(minute3>59) {
-            hour3 += 1
-            minute3 -= 60
-        }
-        if(hour3>23) {
-            hour3 -= 24
+
+        while(minute3>59){
+            if(minute3>59) {
+                hour3 += 1
+                minute3 -= 60
+            }
+            if(hour3>23) {
+                hour3 -= 24
+            }
         }
         val timeString3 = hour3.toString() + "시" + minute3.toString() + "분"
         binding.timeTextView3.text = timeString3
 
         var hour4 = hour3+1
         var minute4 = minute3+30
-        if(minute4>59) {
-            hour4 += 1
-            minute4 -= 60
-        }
-        if(hour4>23) {
-            hour4 -= 24
+
+        while(minute4>59){
+            if(minute4>59) {
+                hour4 += 1
+                minute4 -= 60
+            }
+            if(hour4>23) {
+                hour4 -= 24
+            }
         }
         val timeString4 = hour4.toString() + "시" + minute4.toString() + "분"
         binding.timeTextView4.text = timeString4
 
         var hour5 = hour4+1
         var minute5 = minute4+30
-        if(minute5>59) {
-            hour5 += 1
-            minute5 -= 60
-        }
-        if(hour5>23) {
-            hour5 -= 24
+
+        while(minute5>59){
+            if(minute5>59) {
+                hour5 += 1
+                minute5 -= 60
+            }
+            if(hour5>23) {
+                hour5 -= 24
+            }
         }
         val timeString5 = hour5.toString() + "시" + minute5.toString() + "분"
         binding.timeTextView5.text = timeString5
--- teamproject/app/src/main/java/com/example/myapplication/RecommendupActivity2.kt
+++ teamproject/app/src/main/java/com/example/myapplication/RecommendupActivity2.kt
@@ -24,21 +24,23 @@ class RecommendupActivity2 : AppCompatActivity() {
 
     private fun initlayout() {
 
-        //var sleeptime = dbHelper.selectSleepTime()
-        //Log.d("SleepTime", sleeptime.toString())
-        //database에서 시간 어케 받아와!!!
-
+        dbHelper = DBHelper(this, "dysw.db", null, 1)
+        val sleepTime = dbHelper.getTimeFallSleep()
+        Log.d("sleeptime", sleepTime.toString())
 
         var hour = intent.getIntExtra("hour", 0)
-        var minute = intent.getIntExtra("minute", 0) + 17//평균 잠드는데 까지 걸리는 시간 더해주면 됨.
+        var minute = intent.getIntExtra("minute", 0) + sleepTime//평균 잠드는데 까지 걸리는 시간 더해주면 됨.
 
-        if(minute>59) {
-            hour += 1
-            minute -= 60
+        while(minute>59){
+            if(minute>59) {
+                hour += 1
+                minute -= 60
+            }
+            if(hour>23) {
+                hour -= 24
+            }
         }
-        if(hour>23) {
-            hour -= 24
-        }
+
 
         val timeString = hour.toString() + "시 " + minute.toString() + "분"
 
@@ -46,48 +48,60 @@ class RecommendupActivity2 : AppCompatActivity() {
 
         var hour2 = hour+1
         var minute2 = minute+30
-        if(minute2>59) {
-            hour2 += 1
-            minute2 -= 60
-        }
-        if(hour2>23) {
-            hour2 -= 24
+
+        while(minute2>59){
+            if(minute2>59) {
+                hour2 += 1
+                minute2 -= 60
+            }
+            if(hour2>23) {
+                hour2 -= 24
+            }
         }
         val timeString2 = hour2.toString() + "시" + minute2.toString() + "분"
         binding.timeTextView2.text = timeString2
 
         var hour3 = hour2+1
         var minute3 = minute2+30
-        if(minute3>59) {
-            hour3 += 1
-            minute3 -= 60
-        }
-        if(hour3>23) {
-            hour3 -= 24
+
+        while(minute3>59){
+            if(minute3>59) {
+                hour3 += 1
+                minute3 -= 60
+            }
+            if(hour3>23) {
+                hour3 -= 24
+            }
         }
         val timeString3 = hour3.toString() + "시" + minute3.toString() + "분"
         binding.timeTextView3.text = timeString3
 
         var hour4 = hour3+1
         var minute4 = minute3+30
-        if(minute4>59) {
-            hour4 += 1
-            minute4 -= 60
-        }
-        if(hour4>23) {
-            hour4 -= 24
+
+        while(minute4>59){
+            if(minute4>59) {
+                hour4 += 1
+                minute4 -= 60
+            }
+            if(hour4>23) {
+                hour4 -= 24
+            }
         }
         val timeString4 = hour4.toString() + "시" + minute4.toString() + "분"
         binding.timeTextView4.text = timeString4
 
         var hour5 = hour4+1
         var minute5 = minute4+30
-        if(minute5>59) {
-            hour5 += 1
-            minute5 -= 60
-        }
-        if(hour5>23) {
-            hour5 -= 24
+
+        while(minute5>59){
+            if(minute5>59) {
+                hour5 += 1
+                minute5 -= 60
+            }
+            if(hour5>23) {
+                hour5 -= 24
+            }
         }
         val timeString5 = hour5.toString() + "시" + minute5.toString() + "분"
         binding.timeTextView5.text = timeString5
