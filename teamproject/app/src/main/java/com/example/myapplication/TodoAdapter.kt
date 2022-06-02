8d4d3c75bf56a665496846db4078e8db93464d22
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-06-02 19:15:40 +0900

    commit for pull

--- teamproject/app/src/main/java/com/example/myapplication/TodoAdapter.kt
+++ teamproject/app/src/main/java/com/example/myapplication/TodoAdapter.kt
@@ -3,13 +3,27 @@ package com.example.myapplication
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.TodorowBinding
 
 class TodoAdapter(val items:ArrayList<Tododata>) : RecyclerView.Adapter<TodoAdapter.ViewHolder>(){
+
+    interface OnItemClickListener{
+        fun OnItemClick(data:Tododata,pos:Int)
+    }
+    var itemClickListener:OnItemClickListener?=null
+
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
         val textView = itemView.findViewById<TextView>(R.id.todotextView)
+        val checkbutton = itemView.findViewById<ImageView>(R.id.checkbutton)
+        init{
+            checkbutton.setOnClickListener {
+                //text정보, view정보도 줄 생각
+                itemClickListener?.OnItemClick(items[adapterPosition],adapterPosition)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
--- teamproject/app/src/main/java/com/example/myapplication/TodoFragment.kt
+++ teamproject/app/src/main/java/com/example/myapplication/TodoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -12,6 +13,7 @@ class TodoFragment : Fragment() {
     private var data:ArrayList<Tododata> = ArrayList()
     lateinit var adapter:TodoAdapter
     lateinit var recyclerView: RecyclerView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,19 +25,41 @@ class TodoFragment : Fragment() {
         initData()
         recyclerView.layoutManager = LinearLayoutManager(activity)
         adapter = TodoAdapter(data)
+        adapter.itemClickListener = object : TodoAdapter.OnItemClickListener{
+            override fun OnItemClick(data: Tododata, pos: Int) {
+                Toast.makeText(recyclerView.context,data.textString,Toast.LENGTH_SHORT).show()
+            }
+        }
         recyclerView.adapter = adapter
 
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    // activity에 정보 전달해준다.
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        // 정보 수신 fragment 간에는 통신 어려우니까 activity를 통해서 통신
+//        if(activity!= null){
+//            val intent = activity?.intent
+//            if(intent!=null){
+//                val str = intent.getStringExtra("name")
+//            }
+//        }
+    }
+
     private fun initData() {
+        // DB에 저장된 값들 읽어오기
         data.add(Tododata("씻기"))
-        data.add(Tododata("데이트하기"))
+        data.add(Tododata("후하후하"))
         data.add(Tododata("양치하기"))
-        data.add(Tododata("양치하기"))
-        data.add(Tododata("양치하기"))
-        data.add(Tododata("양치하기"))
-        data.add(Tododata("양치하기"))
-        data.add(Tododata("양치하기"))
+        data.add(Tododata("배고파"))
+        data.add(Tododata("뿌링클"))
+        data.add(Tododata("허니콤보"))
+        data.add(Tododata("돈냉"))
+        data.add(Tododata("루나코인"))
     }
 }
