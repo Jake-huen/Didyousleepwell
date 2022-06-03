bb8c594957a5c7c4da198225fff20c496c3b8adf
jeongHanByeol <jhb990438@gmail.com>
2022-06-03 10:40:21 +0900

    권한 허용 버튼 추가

--- teamproject/app/src/main/java/com/example/myapplication/InitFragment2.kt
+++ teamproject/app/src/main/java/com/example/myapplication/InitFragment2.kt
@@ -5,12 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class InitFragment2 : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_init2, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_init2, container, false)
+        val btn_permission = view.findViewById<Button>(R.id.btn_permission)
+
+
+        btn_permission.setOnClickListener {
+
+        }
+
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
